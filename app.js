e67af1b9395f3d34cf843273d99e7790a1dd6f83
Norman Witte III <norm.witte@gmail.com>
2018-04-23 10:47:36 -0400

    Simple buy alert with pm2 startup json

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const config = require('./config.json');
 const api = require('binance');
+const TelegramBot = require('node-telegram-bot-api');
 const binance = new api.BinanceRest(config.binance);
 const socket = new api.BinanceWS(true);
+const bot = new TelegramBot(config.telegram.token, { polling: false });
 
-// First Commit: Monitor user data for result formatting
+// Socket listens for new Binance activity and sends alerts for new buy orders via Telegram
 socket.onUserData(binance, (res) => {
-  console.log(res);
+  if (res.eventType == 'executionReport' && res.side == 'BUY' && res.orderStatus == 'NEW') {
+    var cost = parseFloat(res.price) * parseFloat(res.quantity);
+    var resp  = "BINANCE%0A";
+    resp     += "Just bought: " + res.symbol + "%0A";
+    resp     += "Rate: " + res.price + "%0A";
+    resp     += "Amount: " + res.quantity + "%0A";
+    resp     += "Cost: " + cost.toFixed(8);
+    bot.sendMessage(config.telegram.chatId, resp);
+  }
 });
